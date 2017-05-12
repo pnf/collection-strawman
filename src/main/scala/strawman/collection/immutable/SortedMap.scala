4bc4f27d12875125972ffc90b873188ba9f5ee02
Julien Richard-Foy <julien@richard-foy.fr>
2017-05-12 20:48:33 +0200

    Fix compilation of core on Dotty

--- src/main/scala/strawman/collection/immutable/SortedMap.scala
+++ src/main/scala/strawman/collection/immutable/SortedMap.scala
@@ -9,9 +9,11 @@ trait SortedMap[K, +V]
      with collection.SortedMap[K, V]
      with SortedMapOps[K, V, SortedMap, SortedMap[K, V]]
 
-trait SortedMapOps[K, +V, +CC[X, +Y] <: SortedMap[X, Y] with SortedMapOps[X, Y, CC, _], +C <: SortedMap[K, V]]
+trait SortedMapOps[K, +V, +CC[X, Y] <: SortedMap[X, Y] with SortedMapOps[X, Y, CC, _], +C <: SortedMap[K, V]]
   extends MapOps[K, V, Map, C]
      with collection.SortedMapOps[K, V, CC, C] {
+
+  protected def coll: SortedMap[K, V]
 
   override def + [V1 >: V](kv: (K, V1)): CC[K, V1] = updated(kv._1, kv._2)
   def updated[V1 >: V](key: K, value: V1): CC[K, V1]
